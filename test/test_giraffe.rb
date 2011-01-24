6463c3cfa0eb4bb29d7f091d82f161a18526888a
Michael Maloney <maloney.mc@gmail.com>
2011-01-23 19:25:21 -0500

    no more tests and adding launchy to dependencies

--- test/test_giraffe.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-class TestGiraffe < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "GIRAFFES ARE PERFECT AND DO NOT REQUIRE TESTING."
-  end
-end
